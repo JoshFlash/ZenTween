7d528ebbd9d7c06ff52b73bb00a13cc8fe895716
Joshua Flash <joshuaflash@gmail.com>
2021-04-13 13:10:19 +1200

    add stride helper methods for scale and rotation tweens

--- Pleasing/StrideTweening.cs
+++ Pleasing/StrideTweening.cs
@@ -47,12 +47,50 @@ namespace Pleasing
 			var keyFrame = new TweenKeyFrame<Quaternion>(duration * FromSeconds, rotation, GetEasingFunction(easingType));
 			Tween(transform, ROTATION, keyFrame, lerpFunction ?? LerpFunctions.Quaternion, delay * FromSeconds, onComplete);
 		}
+		
+		public static void TweenLoopRotate(this TransformComponent transform, Quaternion rotation, float duration,
+			EasingType easingTypeIn = EasingType.SinusoidalInOut, EasingType easingTypeOut = EasingType.SinusoidalInOut, float delay = 0, System.Action onComplete = null,
+			LerpFunction<Quaternion> lerpFunction = null)
+		{
+			var keyFrameIn = new TweenKeyFrame<Quaternion>((duration / 2) * FromSeconds, rotation, GetEasingFunction(easingTypeIn));
+			var keyFrameOut = new TweenKeyFrame<Quaternion>(duration * FromSeconds, transform.Rotation, GetEasingFunction(easingTypeOut));
+			TweenLoop(transform, ROTATION, keyFrameIn, keyFrameOut, lerpFunction ?? LerpFunctions.Quaternion, delay * FromSeconds, onComplete);
+		}
 
+		public static void TweenLoopRotateSequence(this TransformComponent transform, TweenSequence<Quaternion> tweenSequence, TweenSequence<Quaternion> introSequence = null)
+		{
+			TweenLoopSequence(transform, ROTATION, tweenSequence, introSequence);
+		}
+
+		public static void TweenRotateSequence(this TransformComponent transform, TweenSequence<Quaternion> tweenSequence)
+		{
+			TweenOneShotSequence(transform, ROTATION, tweenSequence);
+		}
+		
 		public static void TweenScale(this TransformComponent transform, Vector3 scale, float duration,
 			EasingType easingType = EasingType.CubicInOut, float delay = 0, System.Action onComplete = null, LerpFunction<Vector3> lerpFunction = null)
 		{
 			var keyFrame = new TweenKeyFrame<Vector3>(duration * FromSeconds, scale, GetEasingFunction(easingType));
 			Tween(transform, SCALE, keyFrame, lerpFunction ?? LerpFunctions.Vector3, delay * FromSeconds, onComplete);
+		}
+		
+		public static void TweenLoopScale(this TransformComponent transform, Vector3 scale, float duration,
+			EasingType easingTypeIn = EasingType.SinusoidalInOut, EasingType easingTypeOut = EasingType.SinusoidalInOut, float delay = 0, System.Action onComplete = null, 
+			LerpFunction<Vector3> lerpFunction = null)
+		{
+			var keyFrameIn = new TweenKeyFrame<Vector3>((duration / 2) * FromSeconds, scale, GetEasingFunction(easingTypeIn));
+			var keyFrameOut = new TweenKeyFrame<Vector3>(duration * FromSeconds, transform.Scale, GetEasingFunction(easingTypeOut));
+			TweenLoop(transform, SCALE, keyFrameIn, keyFrameOut, lerpFunction ?? LerpFunctions.Vector3, delay * FromSeconds, onComplete);
+		}
+		
+		public static void TweenLoopScaleSequence(this TransformComponent transform, TweenSequence<Vector3> tweenSequence, TweenSequence<Vector3> introSequence = null)
+		{
+			TweenLoopSequence(transform, SCALE, tweenSequence, introSequence);
+		}
+
+		public static void TweenScaleSequence(this TransformComponent transform, TweenSequence<Vector3> tweenSequence)
+		{
+			TweenOneShotSequence(transform, SCALE, tweenSequence);
 		}
 #endif
 
